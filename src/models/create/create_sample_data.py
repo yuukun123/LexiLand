ee9f279d5f9e74ccb7dc19ebca7c6f2cea5ad6d4
Yuu <152776955+yuukun123@users.noreply.github.com>
2025-08-26 17:18:36 +0700

    update file

--- src/models/create/create_sample_data.py
+++ src/models/create/create_sample_data.py
@@ -35,7 +35,22 @@ class CreateSampleData:
     def create_sample_data(self):
         self.cursor.execute("""
                 INSERT INTO topics (topic_name, created_at, user_id)
-                VALUES ('Other', datetime('now'), 1)
+                VALUES ('Topic 1', datetime('now'), 1),
+                        ('Topic 1', datetime('now'), 2),
+                        ('Topic 2', datetime('now'), 1),
+                        ('Topic 2', datetime('now'), 2),
+                        ('Topic 3', datetime('now'), 1),
+                        ('Topic 3', datetime('now'), 2),
+                        ('Topic 4', datetime('now'), 1),
+                        ('Topic 4', datetime('now'), 2),
+                        ('Topic 5', datetime('now'), 1),
+                        ('Topic 5', datetime('now'), 2),
+                        ('Topic 6', datetime('now'), 1),
+                        ('Topic 6', datetime('now'), 2),
+                        ('Topic 7', datetime('now'), 1),
+                        ('Topic 7', datetime('now'), 2),
+                        ('Topic 8', datetime('now'), 1),
+                        ('Topic 8', datetime('now'), 2)
             """)
         self.connection.commit()
         print("Sample data inserted successfully.")
