b3fb46af6b2a121df72ee6b1b16489ece45c43d1
DevOfSgu <haovoanh2005@gmail.com>
2025-09-17 19:07:47 +0700

    Commit local changes before pull

--- src/views/main_view/add_vocab_view.py
+++ src/views/main_view/add_vocab_view.py
@@ -6,6 +6,7 @@ from src.constants.form_mode import FormMode
 from src.controllers.buttonController import buttonController
 from src.controllers.main_controller.add_vocab_controller import AddWordController
 from src.views.moveable_window import MoveableWindow
+from resources import resources_rc
 
 
 class AddWordDialog(QDialog, MoveableWindow):
